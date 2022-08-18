e6f919cce2da35c994f2eafa3d22cf2a5e235069
kadirboylu <kadir.boylu@outlook.com>
2022-08-18 19:17:45 +0300

    Create answer button component

---
+++ src/components/AnswerButton/AnswerButton.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "./AnswerButton.css";
+import { answerBtn } from "../../assets/svgs";
+import { useGameContext } from "../../contexts/GameContext/GameContext";
+
+const AnswerButton = ({ num, clickedBtn, setClickedBtn }) => {
+  const { quiz, question, setQuestion, setAnswer } = useGameContext();
+
+  const handleClick = () => {
+    if (clickedBtn !== undefined) return;
+
+    setClickedBtn(num);
+
+    if (quiz[question]?.answers[num] === quiz[question]?.correctAnswer) {
+      setAnswer(true);
+    } else {
+      setAnswer(false);
+    }
+
+    setTimeout(() => {
+      if (question < quiz.length - 1) {
+        setQuestion(question + 1);
+        setClickedBtn();
+      } else {
+        setQuestion(0);
+        setClickedBtn();
+      }
+    }, 3000);
+  };
+
+  const trueBtnStyle = (() => {
+    return (
+      clickedBtn !== undefined &&
+      quiz[question]?.answers[num] === quiz[question].result &&
+      "answerTrue"
+    );
+  })();
+
+  const clickedBtnStyle = (() => {
+    return num === clickedBtn && "answerClicked";
+  })();
+
+  return (
+    <button
+      onClick={handleClick}
+      className={`btn answer-btn ${clickedBtnStyle} ${trueBtnStyle}`}
+    >
+      {answerBtn(quiz[question]?.answers[num])}
+    </button>
+  );
+};
+
+export default AnswerButton;
