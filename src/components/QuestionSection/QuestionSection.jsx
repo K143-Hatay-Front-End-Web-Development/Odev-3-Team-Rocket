4ebd0b7d106ba0519dea7646867d75dee5d19758
kadirboylu <kadir.boylu@outlook.com>
2022-08-18 19:34:40 +0300

    Add question logic

--- src/components/QuestionSection/QuestionSection.jsx
+++ src/components/QuestionSection/QuestionSection.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { useGameContext } from "../../contexts/GameContext/GameContext";
 import { student, unhappyStudent, happyStudent } from "../../assets/svgs";
+import "./QuestionSection.css";
 
 const QuestionSection = () => {
-  const { quiz, question } = useGameContext();
+  const { quiz, question, answer } = useGameContext();
 
-  return (
-    <div className="student">
-      {student(
-        `${quiz[question]?.number1} ${quiz[question]?.operator} ${quiz[question]?.number2}`
-      )}
-    </div>
-  );
+  const changeStudent = () => {
+    const questionText = `${quiz[question]?.number1} ${quiz[question]?.operator} ${quiz[question]?.number2}`;
+
+    if (answer) {
+      return happyStudent(questionText);
+    } else if (answer === false) {
+      return unhappyStudent(questionText);
+    } else {
+      return student(questionText);
+    }
+  };
+
+  return <div className="student">{changeStudent()}</div>;
 };
 
 export default QuestionSection;
