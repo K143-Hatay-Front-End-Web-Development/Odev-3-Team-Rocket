e9897d4bf96237fffcce7da87fda65d306feef32
Melin Kan <melin.kan@outlook.com>
2022-08-18 20:26:42 +0300

    Add background styles

--- src/routes/GamePage/GamePage.jsx
+++ src/routes/GamePage/GamePage.jsx
@@ -6,28 +6,38 @@ import QuestionSection from "../../components/QuestionSection/QuestionSection";
 import Answers from "../../components/Answers/Answers";
 
 const GamePage = () => {
-  const { quiz, question } = useGameContext();
+  const { quiz, question, answer } = useGameContext();
+
+  const backgroundStyles = (() => {
+    return answer === true
+      ? "bg-answer-true"
+      : answer === false
+      ? "bg-answer-false"
+      : "";
+  })();
 
   return (
-    <div className="container game-wrapper">
-      {quiz[question]?.answers ? (
-        <div>
-          <div className="info">
-            <QuizStats />
+    <div className={`base-gamepage ${backgroundStyles}`}>
+      <div className="container game-wrapper">
+        {quiz[question]?.answers ? (
+          <div>
+            <div className="info">
+              <QuizStats />
+            </div>
+            <div className="game">
+              <QuestionSection />
+              <Answers />
+            </div>
           </div>
-          <div className="game">
-            <QuestionSection />
-            <Answers />
+        ) : (
+          <div className="error">
+            <h1>Go Home and Choose Quiz</h1>
+            <Link className="go-home-btn" to="/">
+              GO HOME
+            </Link>
           </div>
-        </div>
-      ) : (
-        <div className="error">
-          <h1>Go Home and Choose Quiz</h1>
-          <Link className="go-home-btn" to="/">
-            GO HOME
-          </Link>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
