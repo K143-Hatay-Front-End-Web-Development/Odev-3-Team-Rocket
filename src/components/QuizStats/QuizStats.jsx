620a5d3f913072252ce88c774a628aec3bd116cf
kadirboylu <kadir.boylu@outlook.com>
2022-08-18 17:24:21 +0300

    Create quiz stats component

---
+++ src/components/QuizStats/QuizStats.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./QuizStats.css";
+
+const QuizStats = () => {
+  return (
+    <div className="base">
+      <ul>
+        <li>Puan: 120</li>
+        <li>Tur: 2</li>
+        <li>Soru: 7</li>
+      </ul>
+    </div>
+  );
+};
+
+export default QuizStats;
