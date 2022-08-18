2fa9461e0bb7d90695b7ebdb3d0b159f15ea15ff
kadirboylu <kadir.boylu@outlook.com>
2022-08-18 16:42:38 +0300

    Create questions

--- src/routes/GamePage/GamePage.jsx
+++ src/routes/GamePage/GamePage.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import { useState } from "react";
+import { useGameContext } from "../../contexts/GameContext/GameContext";
+import "./GamePage.css";
 
-// SVG
 import {
   student,
   answerBtn,
@@ -8,10 +9,18 @@ import {
   happyStudent,
 } from "../../assets/svgs";
 
-// CSS
-import "./GamePage.css";
-
 const GamePage = () => {
+  const [question, setQuestion] = useState(0);
+  const { quiz } = useGameContext();
+
+  const handleClick = () => {
+    if (question < quiz.length - 1) {
+      setQuestion(question + 1);
+    } else {
+      setQuestion(0);
+    }
+  };
+
   return (
     <div className="container game-wrapper">
       <div className="info">
@@ -22,11 +31,21 @@ const GamePage = () => {
         </ul>
       </div>
       <div className="game">
-        <div className="student">{student("7 x 8")}</div>
+        <div className="student">
+          {student(
+            `${quiz[question].number1} ${quiz[question].operator} ${quiz[question].number2}`
+          )}
+        </div>
         <div className="answers">
-          <button className="btn answer-btn">{answerBtn(49)}</button>
-          <button className="btn answer-btn">{answerBtn(64)}</button>
-          <button className="btn answer-btn">{answerBtn(56)}</button>
+          <button onClick={handleClick} className="btn answer-btn">
+            {answerBtn(quiz[question].answers[0])}
+          </button>
+          <button className="btn answer-btn">
+            {answerBtn(quiz[question].answers[1])}
+          </button>
+          <button className="btn answer-btn">
+            {answerBtn(quiz[question].answers[2])}
+          </button>
         </div>
       </div>
     </div>
